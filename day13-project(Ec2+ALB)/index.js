59372cdd26ba49c493a88e08f5dec4870231e526
sreekarsai08 <sreekarsai08@gmail.com>
2023-08-15 20:37:04 +0530

    Adding Ec2ALB files

--- day13-project(Ec2+ALB)/index.js
+++ day13-project(Ec2+ALB)/index.js
@@ -22,7 +22,7 @@ app.get('/ec2instance', async (req, res) => {
     try {
       // fetch instance id from http://169.254.169.254/latest/meta-data/instance-id
       let response = await axios.get('http://169.254.169.254/latest/meta-data/instance-id');
-      res.send(response.data);
+      res.send({instanceid: response.data});
     } catch (error) {
       console.log(`Error: ${error}`);
       // 400 response code
